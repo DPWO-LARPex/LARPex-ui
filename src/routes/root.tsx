f0cef6a4581606acf4dbf80d3991705e34028231
Adrian Mostowski <adrian.mostowski@bluesoft.com>
2024-03-26 12:15:38 +0100

    fix daisyiu

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -2,7 +2,11 @@ import { useQuery } from '@tanstack/react-query'
 
 function Root() {
 	useQuery({ queryKey: ['example'] })
-	return <></>
+	return (
+		<>
+			<button className="btn btn-primary">test</button>
+		</>
+	)
 }
 
 export default Root
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ import daisyui from 'daisyui'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: [],
+	content: ['./src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {},
 	},
