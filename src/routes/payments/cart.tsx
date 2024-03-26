aa046d9086b919107b4766d36735aeb3532557fa
Adrian Mostowski <akmostowski@gmail.com>
2024-03-26 17:21:29 +0100

    add payment nav

--- src/routes/payments/cart.tsx
+++ src/routes/payments/cart.tsx
@@ -1,31 +1,33 @@
 import { formatCurrencyAmount } from '@/utils'
+import { Link } from 'react-router-dom'
 
 export default function CartRoute() {
 	return (
-		<>
-			<article>
-				<section className="flex flex-col items-start gap-3">
-					<h1 className="text-2xl">Koszyk</h1>
-					<div className="card card-side bg-base-100 shadow-xl">
-						<figure>
-							<img
-								src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
-								alt="Movie"
-							/>
-						</figure>
-						<div className="card-body">
-							<h2 className="card-title">New movie is released!</h2>
-							<p>Click the button to watch on Jetflix app.</p>
-						</div>
+		<article className="w-full flex flex-col">
+			<section className="flex flex-col items-start gap-3">
+				<h1 className="text-2xl">Koszyk</h1>
+				<div className="card card-side bg-base-100 shadow-xl">
+					<figure>
+						<img
+							src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
+							alt="Movie"
+						/>
+					</figure>
+					<div className="card-body">
+						<h2 className="card-title">New movie is released!</h2>
+						<p>Click the button to watch on Jetflix app.</p>
 					</div>
-					<div className="stats shadow">
-						<div className="stat">
-							<div className="stat-title">Total price</div>
-							<div className="stat-value">{formatCurrencyAmount(9123)}</div>
-						</div>
+				</div>
+				<div className="stats shadow">
+					<div className="stat">
+						<div className="stat-title">Total price</div>
+						<div className="stat-value">{formatCurrencyAmount(9123)}</div>
 					</div>
-				</section>
-			</article>
-		</>
+				</div>
+			</section>
+			<Link className="btn btn-primary self-end" to="address">
+				Dalej
+			</Link>
+		</article>
 	)
 }
--- src/routes/payments/index.tsx
+++ src/routes/payments/index.tsx
@@ -1,21 +1,44 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 
 export default function PaymentRoute() {
 	return (
-		<>
+		<article className="w-full h-full flex flex-col items-center">
 			<PaymentNavigation />
-			<Outlet />
-		</>
+			<div className="w-full p-3">
+				<Outlet />
+			</div>
+		</article>
 	)
 }
 
-// const routes = [
-// 	{ path: '/payments', name: 'Koszyk' },
-// 	{ path: '/payments/address', name: 'Adres' },
-// 	{ path: '/payments/card', name: 'Płatność' },
-// 	{ path: '/payments/summary', name: 'Potwierdzenie' },
-// ]
+const routes = [
+	{ path: '/payments', name: 'Koszyk' },
+	{ path: '/payments/address', name: 'Adres' },
+	{ path: '/payments/card', name: 'Płatność' },
+	{ path: '/payments/summary', name: 'Potwierdzenie' },
+]
 
 const PaymentNavigation = () => {
-	return <nav></nav>
+	const location = useLocation()
+	const currentActiveRoute = routes.findIndex(
+		({ path }) => location.pathname === path,
+	)
+
+	return (
+		<nav>
+			<ul className="steps">
+				{routes.map(({ name }, index) => {
+					const isActive = index <= currentActiveRoute
+					return (
+						<li
+							data-content={isActive ? '✓' : ''}
+							className={`step ${isActive ? 'step-primary' : ''}`}
+						>
+							{name}
+						</li>
+					)
+				})}
+			</ul>
+		</nav>
+	)
 }
