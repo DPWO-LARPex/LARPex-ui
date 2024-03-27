dbe28f40fc084571eb817b0f0957d167a6c10129
Wojciech Zieliński <wojtekz.6776@gmail.com>
2024-03-27 08:25:21 +0100

    get data and map games

--- src/routes/games/games.tsx
+++ src/routes/games/games.tsx
@@ -1,22 +1,33 @@
+import gamesData from './data.json'
+import { Link } from 'react-router-dom'
+
 export default function GamesRoute() {
 	return (
 		<div className="list">
-			<div className="flex my-4">
-				<div className="details w-1/4 p-4">
-					<h2>title</h2>
-					<p>author</p>
-					<p>number of players</p>
-					<p>difficulty level</p>
-					<p>status</p>
+			{gamesData.map((game, index) => (
+				<div key={index} className="game flex my-4">
+					<div className="details w-1/4 p-4">
+						<h2>{game.title}</h2>
+						<p>{game.author}</p>
+						<p>{game.numberOfPlayers}</p>
+						<p>{game.difficultyLevel}</p>
+						<p>{game.status}</p>
+					</div>
+					<div className="image w-3/4 relative group">
+						<img
+							className="object-cover object-top h-64 w-full transition duration-500 ease-in-out group-hover:opacity-50"
+							src={game.imageUrl}
+							alt={game.title}
+						/>
+						<button className="absolute top-1/2 left-1/4 transform -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition duration-500 ease-in-out">
+							More info
+						</button>
+						<button className="absolute top-1/2 left-2/3 transform -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition duration-500 ease-in-out">
+							<Link to={`./edit/${game.id}`}>Edit</Link>
+						</button>
+					</div>
 				</div>
-				<div className="image w-3/4">
-					<img
-						className="object-cover object-top h-64 w-full"
-						src="https://image.jimcdn.com/app/cms/image/transf/dimension=2080x10000:format=jpg/path/s2217cd0bb1220415/image/i968752087e48ef2a/version/1694723212/greatest-medieval-battles.jpg"
-						alt=""
-					/>
-				</div>
-			</div>
+			))}
 		</div>
 	)
 }
