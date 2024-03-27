d62b7092e0b2d23c0164cdb882c9090a06834682
Wojciech Zieliński <wojtekz.6776@gmail.com>
2024-03-27 08:34:12 +0100

    new game button

--- src/routes/games/games.tsx
+++ src/routes/games/games.tsx
@@ -3,7 +3,12 @@ import { Link } from 'react-router-dom'
 
 export default function GamesRoute() {
 	return (
-		<div className="list">
+		<div className="list my-12">
+			<Link to="./create">
+				<div className="addGame flex justify-center items-center h-20 border-2 border-gray-300 bg-black hover:bg-gray-700">
+					Add new game
+				</div>
+			</Link>
 			{gamesData.map((game, index) => (
 				<div key={index} className="game flex my-4">
 					<div className="details w-1/4 p-4">
@@ -20,7 +25,7 @@ export default function GamesRoute() {
 							alt={game.title}
 						/>
 						<button className="absolute top-1/2 left-1/4 transform -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition duration-500 ease-in-out">
-							More info
+							<Link to={`./${game.id}`}>More info</Link>
 						</button>
 						<button className="absolute top-1/2 left-2/3 transform -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition duration-500 ease-in-out">
 							<Link to={`./edit/${game.id}`}>Edit</Link>
