161b4b320f7978b77e38e693e906a706985d7e06
Wojciech Zieliński <wojtekz.6776@gmail.com>
2024-03-27 12:15:00 +0100

    add game list styling

--- src/routes/games/games.tsx
+++ src/routes/games/games.tsx
@@ -6,39 +6,43 @@ export default function GamesRoute() {
 	return (
 		<div className="list my-12">
 			<Link to="./create">
-				<div className="addGame flex justify-center items-center h-20 border-2 border-gray-300 bg-black hover:bg-gray-700">
+				<div className="addGame text-stone-300 flex justify-center items-center h-20 border-2 border-stone-300 bg-stone-900 hover:bg-stone-300 hover:text-stone-900">
 					Add new game
 				</div>
 			</Link>
 			{gamesData.map((game, index) => (
-				<div key={index} className="game flex my-4">
-					<div className="details w-1/4 p-4 bg-black">
-						<h2>{game.title}</h2>
-						<p>Author: {game.author}</p>
-						<p>Max. number of players: {game.numberOfPlayers}</p>
-						<div className="flex items-center">
-							<p>Difficulty level:</p>
-							{[...Array(5)].map((_, i) => (
-								<LuSwords
-									key={i}
-									color={i < Number(game.difficultyLevel) ? 'white' : 'grey'}
-									size={20}
-								/>
-							))}
+				<div key={index} className="game flex my-8">
+					<div className="details w-2/5 p-4 bg-stone-900 text-xl">
+						<h2 className="text-xl md:text-5xl text-center text-white tracking-wider">
+							{game.title}
+						</h2>
+						<p className="text-center text-base">By {game.author}</p>
+						<div className="m-6 space-y-2">
+							<p>Max. number of players: {game.numberOfPlayers}</p>
+							<div className="flex items-center">
+								<p className="mr-2">Difficulty level:</p>
+								{[...Array(5)].map((_, i) => (
+									<LuSwords
+										key={i}
+										color={i < Number(game.difficultyLevel) ? 'white' : 'grey'}
+										size={30}
+									/>
+								))}
+							</div>
+							<p>Status: {game.status}</p>
 						</div>
-
-						<p>Status: {game.status}</p>
 					</div>
-					<div className="image w-3/4 relative group">
+
+					<div className="image w-3/5 relative group">
 						<img
 							className="object-cover object-top h-64 w-full transition duration-500 ease-in-out group-hover:opacity-50"
 							src={game.imageUrl}
 							alt={game.title}
 						/>
-						<button className="absolute top-1/2 left-1/4 transform -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition duration-500 ease-in-out">
+						<button className="btn text-stone-200 bg-stone-900 hover:bg-stone-200 hover:border-stone-200 hover:text-stone-900 absolute top-1/2 left-1/3 transform -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition duration-500 ease-in-out">
 							<Link to={`./${game.id}`}>More info</Link>
 						</button>
-						<button className="absolute top-1/2 left-2/3 transform -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition duration-500 ease-in-out">
+						<button className="btn text-stone-200 bg-stone-900 hover:bg-stone-200 hover:border-stone-200 hover:text-stone-900 absolute top-1/2 left-2/3 transform -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition duration-500 ease-in-out">
 							<Link to={`./edit/${game.id}`}>Edit</Link>
 						</button>
 					</div>
