1b981344194836909eebdfce7e146ce2b2b861d5
Wojciech Zieliński <wojtekz.6776@gmail.com>
2024-06-04 12:42:51 +0200

    eventDetails: update display

--- src/routes/events/eventDetails.tsx
+++ src/routes/events/eventDetails.tsx
@@ -1,9 +1,8 @@
-import { runEventAction, sendQuestion } from '@/model/events'
+import { runEventAction } from '@/model/events'
 import { EventPostSchema } from '@/model/events/types'
 import { PlaceGetSchema } from '@/model/places/types'
 import { formatCurrencyAmount } from '@/utils'
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
-import { useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 type EventStatus = {
@@ -14,7 +13,6 @@ type EventStatus = {
 export default function EventDetails() {
 	const { id = '0' } = useParams()
 
-	const [isHintViewOpen, setHintViewOpen] = useState(false)
 	const queryClient = useQueryClient()
 	const eventQuery = useQuery<EventPostSchema>({
 		queryKey: ['api/event', id],
@@ -29,12 +27,6 @@ export default function EventDetails() {
 		enabled: Boolean(event),
 	})
 
-	const [question, setQuestion] = useState('')
-
-	const questionMutation = useMutation({
-		mutationFn: sendQuestion,
-	})
-
 	const eventActionMutation = useMutation({
 		mutationFn: runEventAction,
 		onSuccess: () => {
@@ -44,15 +36,6 @@ export default function EventDetails() {
 		},
 	})
 
-	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault()
-		questionMutation.mutate({
-			event_id: Number(id) ?? 0,
-			user_id: 1,
-			content: question,
-		})
-	}
-
 	if (!eventQuery.isSuccess) return <div>Loading...</div>
 
 	const eventStatus = getEventStatus(eventStatusQuery.data?.name)
@@ -60,15 +43,15 @@ export default function EventDetails() {
 	const numericId = Number(id)
 
 	return (
-		<div className="my-12 px-28 py-12 flex flex-col gap-3">
+		<div className="my-12 flex flex-col gap-3 bg-stone-900">
 			<div className="flex flex-col gap-8 items-center">
 				<img className="w-full h-[200px]" src={event?.icon} alt={event?.icon} />
 				<div className="flex gap-2 p-3 bg-slate-800">
 					Status: <span>{eventStatus?.children}</span>{' '}
 					<div className={`${eventStatus?.color}  w-6 h-6 rounded-full`} />
 				</div>
-				<div className="flex w-full gap-5">
-					<div className="flex flex-col gap-5 flex-1">
+				<div className="flex w-full gap-5 px-12">
+					<div className="flex flex-col gap-5 flex-1 bg-slate-800  p-3 ">
 						<div className=" flex flex-col gap-3">
 							<p>Data i godzina rozpoczęcia</p>
 							<p>{event?.date}</p>
@@ -97,32 +80,16 @@ export default function EventDetails() {
 						</div>
 					</div>
 				</div>
-				<div className="flex w-full gap-5">
-					<div className="flex flex-col gap-5 flex-1">
+				<div className="flex w-full gap-5 px-12 pb-12">
+					<div className="flex flex-col gap-5 flex-1 bg-slate-800  p-3 ">
 						<p>Lista graczy</p>
 						...
 					</div>
-					<div className="flex flex-col w-3/5 gap-5 bg-slate-800 p-3">
+					<div className="flex flex-col w-3/5 gap-5 p-3">
 						<div className="flex flex-col gap-3">
-							<button
-								className="btn"
-								onClick={() => setHintViewOpen(prev => !prev)}
-							>
-								{isHintViewOpen ? 'Zakończ prośbę' : 'Prośba o podpowiedź'}
+							<button className="btn bg-red-600 hover:bg-red-800 text-white">
+								Lista podpowiedzi
 							</button>
-							{questionMutation.isSuccess ? <p>Wysłano prośbę</p> : null}
-							{isHintViewOpen ? (
-								<form onSubmit={handleSubmit}>
-									<textarea
-										value={question}
-										onChange={e => setQuestion(e.target.value)}
-										className="textarea"
-									></textarea>
-									<button className="btn" type="submit">
-										Wyślij
-									</button>
-								</form>
-							) : null}
 						</div>
 						<div className="flex justify-between">
 							<button
