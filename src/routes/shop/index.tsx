567493b4efa1340abfa0b9f3141162b41ed17196
Adrian Mostowski <akmostowski@gmail.com>
2024-06-04 20:21:59 +0200

    add micropayment payment

--- src/routes/shop/index.tsx
+++ src/routes/shop/index.tsx
@@ -1,10 +1,11 @@
 import PaymentMethod from '@/components/PaymentMethod'
+import { usePayment } from '@/context/PaymentContext'
 import { useQuery } from '@tanstack/react-query'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 type MicrostoreItems = {
-	id: number
+	item_id: number
 	name: string
 	description: string
 	price: number
@@ -12,8 +13,9 @@ type MicrostoreItems = {
 
 export default function Shop() {
 	const navigate = useNavigate()
+	const { dispatch } = usePayment()
 	const [selectedItem, setSelectedItem] = useState<number | null>(null)
-	const [paymentId, setPaymentId] = useState<number>(0)
+	const [paymentId, setPaymentId] = useState<number | null>(null)
 
 	const storeItemsQuery = useQuery<MicrostoreItems[]>({
 		queryKey: ['api/microstore/items'],
@@ -27,22 +29,38 @@ export default function Shop() {
 	}
 
 	const handleNext = () => {
+		const selectedItemData = storeItemsQuery.data?.find(
+			({ item_id }) => item_id === selectedItem,
+		)
+
+		dispatch({
+			type: 'setPaymentSetup',
+			payload: {
+				data: {
+					amount: 100,
+					payment_target: 'microitem',
+					payment_target_id: Number(selectedItemData?.item_id),
+					payment_method_id: 1,
+					user_id: 1,
+					date: new Date().toISOString(),
+				},
+			},
+		})
 		navigate('/payments')
 	}
 
 	return (
 		<div className="my-12 px-28 py-12 flex flex-col gap-3">
 			<div className="flex gap-5 justify-between flex-wrap">
-				{storeItemsQuery.data?.map(({ name, price, id, description }) => (
-					<div className="card w-96 bg-base-100 shadow-xl" key={id}>
+				{storeItemsQuery.data?.map(({ name, item_id, description }) => (
+					<div className="card w-96 bg-base-100 shadow-xl" key={item_id}>
 						<div className="card-body">
 							<h2 className="card-title">{name}</h2>
 							<h3 className="card-title">{description}</h3>
-							<p>{price}$</p>
 							<div className="card-actions justify-start">
 								<button
-									className={`btn btn-teritary ${selectedItem === id ? 'bg-red-500 hover:bg-red-600' : ''}`}
-									onClick={() => handleSelectItem(id)}
+									className={`btn btn-teritary ${selectedItem === item_id ? 'bg-red-500 hover:bg-red-600' : ''}`}
+									onClick={() => handleSelectItem(item_id)}
 								>
 									Wybierz
 								</button>
@@ -55,7 +73,7 @@ export default function Shop() {
 			<PaymentMethod paymentId={paymentId} setPaymentId={setPaymentId} />
 			<button
 				className="btn bg-red-500 hover:bg-red-600"
-				disabled={!selectedItem || !paymentId}
+				disabled={isNil(selectedItem) || isNil(paymentId)}
 				onClick={handleNext}
 			>
 				Dalej
@@ -63,3 +81,5 @@ export default function Shop() {
 		</div>
 	)
 }
+
+const isNil = (value: unknown) => value === null || value === undefined
